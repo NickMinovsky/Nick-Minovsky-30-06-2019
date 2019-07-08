e6da90852b384437bd3104a6f8d4794c911c1097
NickMinovsky <nickminovsky@gmail.com>
2019-07-08 17:03:04 +0300

    fixed issues

--- src/components/Layout/Footer/Footer.js
+++ src/components/Layout/Footer/Footer.js
@@ -5,19 +5,32 @@ const Footer = () => {
   return (
     <footer className="footer-distributed">
       <div className="footer-right">
-        <a href="https://www.facebook.com/" target="_blank">
+        <a
+          href="https://www.facebook.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <i className="fa fa-facebook" />
         </a>
-        <a href="https://www.twitter.com/" target="_blank">
+        <a
+          href="https://www.twitter.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <i className="fa fa-twitter" />
         </a>
         <a
           href="https://il.linkedin.com/in/nick-minovsky-557383184"
           target="_blank"
+          rel="noopener noreferrer"
         >
           <i className="fa fa-linkedin" />
         </a>
-        <a href="https://github.com/nickminovsky" target="_blank">
+        <a
+          href="https://github.com/nickminovsky"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <i className="fa fa-github" />
         </a>
       </div>
